6ccada4a7f1e41bb4ff663b32b1fac6c21cb0f1c
Geonsoon <qkrrjstns23@gmail.com>
2024-06-17 23:46:50 +0900

    email error modify

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -10,7 +10,7 @@ with DAG(
 ) as dag:
     send_email_task = EmailOperator(
         task_id='send_email_task',
-        to='qkrrjstns23@gamil.com',
+        to='qkrrjstns23@gmail.com',
         subject='Airflow 성공메일',
         html_content='Airflow 작업이 완료되었습니다'
     )
